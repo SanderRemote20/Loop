24c8a909bd4fd6f0dd288734507b5bead0ff1d32
Marius Haugen <63503509+SanderRemote20@users.noreply.github.com>
2024-09-20 17:24:04 +0200

    Update FavoriteFoodsView.swift

--- Loop/Views/FavoriteFoodsView.swift
+++ Loop/Views/FavoriteFoodsView.swift
@@ -61,7 +61,7 @@ struct FavoriteFoodsView: View {
                     dismissButton
                 }
             }
-            .navigationBarTitle("Favorite Foods", displayMode: .large)
+            .navigationBarTitle("Favorittmat", displayMode: .large)
         }
         .sheet(isPresented: $viewModel.isAddViewActive) {
             AddEditFavoriteFoodView(onSave: viewModel.onFoodSave(_:))
